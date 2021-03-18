cf8f3a52f53c9e4b687f74b0f304916598b22783
Your Name <you@example.com>
2021-03-18 14:29:16 -0400

    added models to server.js file

--- Develop/server.js
+++ Develop/server.js
@@ -3,6 +3,13 @@ const routes = require('./routes');
 // import sequelize connection
 const sequelize = require('./config/connection');
 
+// Import model to sync table with database
+const Category = require('./models/Category');
+// const index = require('./models/index');
+const Product = require('./models/Product');
+const ProductTag = require('./models/ProductTag');
+const Tag = require('./models/Tag');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,7 +20,7 @@ app.use(routes);
 
 
 // Force true to drop/recreate table(s) on every sync
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
 
